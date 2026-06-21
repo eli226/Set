1edc03c185871253632b005010e8b2f813490ee3
eldar <eldardzhunaev@gmail.com>
2026-06-21 15:04:37 +0500

    add empty test

---
+++ build/CMakeFiles/set.dir/tests/test_empty.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/set.dir/tests/test_empty.cpp.o: \
+  /Users/eldar/CLionProjects/practirim/set/tests/test_empty.cpp
---
+++ build/CMakeFiles/set.dir/tests/test_iterator.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/set.dir/tests/test_iterator.cpp.o: \
+  /Users/eldar/CLionProjects/practirim/set/tests/test_iterator.cpp
